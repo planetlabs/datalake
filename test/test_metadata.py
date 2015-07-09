2c8fc0f0fe9e11b9b477361f3d6e1093616b1a39
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-07-09 16:19:16 -0700

    factor out metadata into its own object

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from atl import Log, InvalidATLMetadata, UnsupportedATLMetadataVersion
+from atl import Metadata, InvalidATLMetadata, UnsupportedATLMetadataVersion
 
 
-class TestFromATLMetadata(TestCase):
+class TestMetadataValidation(TestCase):
 
     def setUp(self):
         self.metadata = {
@@ -17,19 +17,19 @@ class TestFromATLMetadata(TestCase):
     def test_missing_version(self):
         del(self.metadata['version'])
         with self.assertRaises(InvalidATLMetadata):
-            Log.from_atl_metadata(self.metadata, '/dev/null')
+            Metadata(self.metadata)
 
     def test_unsupported_version(self):
         self.metadata['version'] = '100'
         with self.assertRaises(UnsupportedATLMetadataVersion):
-            Log.from_atl_metadata(self.metadata,'/dev/null')
+            Metadata(self.metadata)
 
     def test_normalize_date(self):
         self.metadata['start'] = '2015-03-20'
-        l = Log.from_atl_metadata(self.metadata, '/dev/null')
-        self.assertEqual(l.metadata['start'], '2015-03-20T00:00:00Z')
+        m = Metadata(self.metadata)
+        self.assertEqual(m['start'], '2015-03-20T00:00:00Z')
 
     def test_invalid_date(self):
         self.metadata['end'] = 'bxfl230'
         with self.assertRaises(InvalidATLMetadata):
-            Log.from_atl_metadata(self.metadata, '/dev/null')
+            Metadata(self.metadata)
