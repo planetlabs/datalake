d38342f8ced8e46c87bd383e8aead7a0e52c1b70
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-07-23 19:25:03 -0700

    make url something created by atl instead of part of metadata
    Having the url as part of the metadata introduces a
    chicken-and-egg problem. Instead of developing some painful
    workflow to enable this idea, just make the url an external
    thing.

--- atl/log.py
+++ atl/log.py
@@ -1,13 +1,21 @@
 from metadata import Metadata
+import os
 
 
-class Log(file):
+class Log(object):
+    '''A Log to be manipulated by the Archive
 
-    @classmethod
-    def from_atl_metadata(cls, metadata, path):
+    A Log is a file and its associated metadata. To create a new Log, you must
+    provide the path to the log and the required Metadata.
+    '''
+    def __init__(self, path, metadata):
         if type(metadata) is not Metadata:
             metadata = Metadata(metadata)
-        log = cls(path)
-        log.metadata = metadata
-        log.path = path
-        return log
+        self.metadata = metadata
+        self._fd = open(path, 'r')
+        self.name = os.path.basename(path)
+        self._initialize_methods_from_fd()
+
+    def _initialize_methods_from_fd(self):
+        for m in ['read', 'readlines', 'seek', 'tell', 'close']:
+            setattr(self, m, getattr(self._fd, m))
