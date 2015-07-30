2bad549d2a54e45135945b6ebef6884748e23fb9
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-07-29 17:11:59 -0700

    use recommended practices for dependency management

--- setup.py
+++ setup.py
@@ -3,23 +3,28 @@ from setuptools import distutils
 from pip.req import parse_requirements
 from pip.download import PipSession
 import os
+import sys
 
-def get_requirements():
-    path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-    reqs = []
-    if os.path.exists(path):
-        reqs = parse_requirements(path, session=PipSession())
-        reqs = [str(r.req) for r in reqs]
-    return reqs
+def get_version_from_pkg_info():
+    metadata = distutils.dist.DistributionMetadata("PKG-INFO")
+    return metadata.version
+
+def get_version_from_pyver():
+    try:
+        import pyver
+    except ImportError:
+        if 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
+            raise ImportError('You must install pyver to create a package')
+        else:
+            return 'noversion'
+    version, version_info = pyver.get_version(pkg="datalake", public=True)
+    return version
 
 def get_version():
     if os.path.exists("PKG-INFO"):
-        metadata = distutils.dist.DistributionMetadata("PKG-INFO")
-        return metadata.version
+        return get_version_from_pkg_info()
     else:
-        import pyver
-        version, version_info = pyver.get_version(pkg="datalake", public=True)
-        return version
+        return get_version_from_pyver()
 
 setup(name='datalake',
       url='https://github.com/planetlabs/datalake',
@@ -28,6 +33,23 @@ setup(name='datalake',
       author='Brian Cavagnolo',
       author_email='brian@planet.com',
       packages=['datalake'],
-      install_requires=get_requirements(),
+      install_requires=[
+          'python-dateutil>=2.4.2',
+          'pytz>=2015.4',
+          'pyver>=1.0.18',
+          'ConfigArgParse>=0.9.3',
+          'boto>=2.38.0',
+          'memoized_property>=1.0.2',
+          'simplejson>=3.7',
+      ],
+      extras_require={
+          'test': [
+              'nose==1.3.6',
+              'moto==0.4.2',
+              'twine==1.5.0',
+              'pip==7.1.0',
+              'wheel==0.24.0',
+          ]
+      },
       scripts=['bin/datalake'],
      )
