e512e2e54d52977e722abdf75efd74f97deacc24
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-07-30 14:27:31 -0700

    give File and id
    This will help us name things in a way that does not collide, and
    also prevent duplicate files.

--- datalake/dlfile.py
+++ datalake/dlfile.py
@@ -1,6 +1,7 @@
 from metadata import Metadata
 import os
-
+from memoized_property import memoized_property
+from pyblake2 import blake2b
 
 class File(object):
     '''A File to be manipulated by the Archive
@@ -13,9 +14,28 @@ class File(object):
             metadata = Metadata(metadata)
         self.metadata = metadata
         self._fd = open(path, 'r')
+        self._path = path
         self.name = os.path.basename(path)
         self._initialize_methods_from_fd()
 
     def _initialize_methods_from_fd(self):
         for m in ['read', 'readlines', 'seek', 'tell', 'close']:
             setattr(self, m, getattr(self._fd, m))
+
+    _HASH_BUF_SIZE = 65536
+
+    def _calculate_hash(self):
+        # this takes just under 2s on my laptop for a 1GB file.
+        b2 = blake2b()
+        with open(self._path, 'rb') as f:
+            while True:
+                data = f.read(self._HASH_BUF_SIZE)
+                if not data:
+                    break
+                b2.update(data)
+        return b2.hexdigest()
+
+    @memoized_property
+    def id(self):
+        '''a unique id for this file'''
+        return self._calculate_hash()
---
+++ test/test_file.py
@@ -0,0 +1,42 @@
+import pytest
+import random
+import string
+from datetime import datetime, timedelta
+
+from datalake import File
+
+def random_word(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+def random_interval():
+    now = datetime.now()
+    start = now - timedelta(days=random.randint(0, 365*3))
+    end = start - timedelta(days=random.randint(1, 10))
+    return start.isoformat(), end.isoformat()
+
+def random_metadata():
+    start, end = random_interval()
+    return {
+        'version': '0',
+        'start': start,
+        'end': end,
+        'where': random_word(10),
+        'what': random_word(10),
+    }
+
+def random_file(tmpdir):
+    name = random_word(10)
+    content = random_word(256)
+    f = tmpdir.join(name)
+    f.write(content)
+    return File(f.strpath, random_metadata())
+
+@pytest.fixture
+def random_files(tmpdir):
+    def get_randfiles(n):
+        return [random_file(tmpdir) for _ in range(n)]
+    return get_randfiles
+
+def test_file_id_probably_unique(random_files):
+    files = random_files(2)
+    assert files[0].id != files[1].id
