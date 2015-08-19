1e4f9cdbf1ffccd936117d76f66959c9360edf90
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-08-18 17:31:28 -0700

    make dynamodb start on boot in vagrant env

--- scripts/dynamodb.sh
+++ scripts/dynamodb.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DYNAMODB_HOME=~/opt/dynamodb
+DYNAMODB_HOME=/opt/dynamodb
 LOG=/var/log/dynamodb.log
 
 echo '=========================' >> ${LOG}
 date >> ${LOG}
 cd ${DYNAMODB_HOME}
-java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb >> ${LOG}
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb >> ${LOG} 2>&1
--- scripts/install_dynamodb.sh
+++ scripts/install_dynamodb.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-DYNAMODB_HOME=~/opt/dynamodb
+DYNAMODB_HOME=/opt/dynamodb
 DYNAMODB_FILE=dynamodb_local_latest.tar.gz
 DYNAMODB_URL=http://dynamodb-local.s3-website-us-west-2.amazonaws.com/${DYNAMODB_FILE}
 DYNAMODB_JAR=DynamoDBLocal.jar
@@ -13,9 +13,11 @@ pushd ${DYNAMODB_HOME}
 
 [ -f ${DYNAMODB_FILE} ] || curl -L -s ${DYNAMODB_URL} -o ${DYNAMODB_FILE}
 [ -f ${DYNAMODB_JAR} ] || tar xzf ${DYNAMODB_FILE}
+cp ${HERE}/dynamodb.sh ./
 
 echo Launching dynamodb
-CMD="start-stop-daemon --oknodo --start --pidfile /var/run/dynamodb.pid -b -m --startas ${HERE}/dynamodb.sh"
-echo $CMD > /etc/rc.local
-/etc/rc.local
+CMD="start-stop-daemon --oknodo --start --pidfile /tmp/dynamodb.pid -b -m --startas ${DYNAMODB_HOME}/dynamodb.sh"
+echo "#!/bin/sh -e" > /etc/rc.local
+echo $CMD >> /etc/rc.local
+/etc/init.d/rc.local start
 popd
