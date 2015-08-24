07f9c6e41ea9f7bb42463d396966b93fcf0daafb
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-08-24 11:04:54 -0700

    address review comments

--- test/__init__.py
+++ test/__init__.py
@@ -17,7 +17,7 @@ def basic_metadata():
     }
 
 def random_word(length):
-    return ''.join(random.choice(string.lowercase) for i in range(length))
+    return ''.join(random.choice(string.lowercase) for i in xrange(length))
 
 def random_interval():
     now = datetime.now()
