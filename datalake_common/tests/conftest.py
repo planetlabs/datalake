4b740ddb11fb5c4b2b29bc6eef0a5569349272f8
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-09-01 12:54:17 -0700

    make random_metadata compliant

--- datalake_common/tests/conftest.py
+++ datalake_common/tests/conftest.py
@@ -19,11 +19,19 @@ def basic_metadata():
 def random_word(length):
     return ''.join(random.choice(string.lowercase) for i in xrange(length))
 
+def random_hex(length):
+    return ('%0' + str(length) + 'x') % random.randrange(16**length)
+
 def random_interval():
     now = datetime.now()
     start = now - timedelta(days=random.randint(0, 365*3))
     end = start - timedelta(days=random.randint(1, 10))
     return start.isoformat(), end.isoformat()
+
+def random_work_id():
+    if random.randint(0, 1):
+        return None
+    return '{}-{}'.format(random_word(5), random.randint(0,2**15))
 
 @pytest.fixture
 def random_metadata():
@@ -32,6 +40,9 @@ def random_metadata():
         'version': 0,
         'start': start,
         'end': end,
+        'work_id': random_work_id(),
         'where': random_word(10),
         'what': random_word(10),
+        'id': random_hex(40),
+        'hash': random_hex(40),
     }
