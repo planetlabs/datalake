97ed9820b4f19f890c613924ecc382bfe419a24e
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-10-29 17:53:29 -0700

    add skeletal queuing capability

--- datalake/__init__.py
+++ datalake/__init__.py
@@ -1,7 +1,8 @@
 import pyver
 
 __version__, __version_info__ = pyver.get_version(pkg='datalake')
-__all__ = ['File', 'Archive']
+__all__ = ['File', 'Archive', 'Uploader', 'Enqueuer']
 
 from dlfile import File
 from archive import Archive
+from queue import Uploader, Enqueuer
