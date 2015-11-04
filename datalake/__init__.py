8d8bec747f59cdbff821c7a0fed90e4c0e7beb0b
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-11-03 19:53:21 -0800

    support crtime and now as valid times

--- datalake/__init__.py
+++ datalake/__init__.py
@@ -1,9 +1,11 @@
 import pyver
 
 __version__, __version_info__ = pyver.get_version(pkg='datalake')
-__all__ = ['File', 'Archive', 'Uploader', 'Enqueuer']
+__all__ = ['File', 'Archive', 'Uploader', 'Enqueuer', 'get_crtime',
+           'CreationTimeError']
 
 from dlfile import File
 from archive import Archive
 from queue import Uploader, Enqueuer
 from translator import Translator, TranslatorError
+from crtime import get_crtime, CreationTimeError
