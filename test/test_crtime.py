06d85ffd921f016cea7838053ab8e2e0df013037
Brian Cavagnolo <brian.cavagnolo@planet.com>
2015-11-17 11:31:48 -0800

    make enqueue support crtime and now like push

--- test/test_crtime.py
+++ test/test_crtime.py
@@ -13,19 +13,10 @@
 # the License.
 
 import pytest
-import os
-import stat
 import time
+from conftest import crtime_setuid
 
 from datalake.crtime import get_crtime, CreationTimeError
-
-
-crtime = os.environ.get('CRTIME', '/usr/local/bin/crtime')
-crtime_available = os.path.isfile(crtime) and os.access(crtime, os.X_OK)
-crtime_setuid = False
-if crtime_available:
-    s = os.stat(crtime)
-    crtime_setuid = s.st_mode & stat.S_ISUID and s.st_uid == 0
 
 
 def test_crtime_does_not_exist(monkeypatch, tmpfile):
