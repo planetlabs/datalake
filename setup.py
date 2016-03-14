e3347872f69e3124a4255cba85ae4cb05a1bc74e
Brian Cavagnolo <brian.cavagnolo@planet.com>
2016-03-14 09:00:44 -0700

    fix and simplify sentry integration

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ setup(name='datalake_api',
           'Flask>=0.10.1',
           'flask-swagger==0.2.8',
           'boto3==1.1.3',
-          'raven>=5.6.0',
+          'raven[flask]>=5.6.0',
+          'blinker>=1.4',
       ],
       extras_require={
           'test': [
