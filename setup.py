84b7c10212fe07d54d882864682457f04309d5be
Nate Parsons <nate@planet.com>
2016-06-30 17:39:26 -0700

    Ports pyver (deprecated) to versioneer

--- setup.py
+++ setup.py
@@ -1,42 +1,26 @@
 from setuptools import setup, find_packages
 from setuptools import distutils
 import os
-import sys
-
-
-def get_version_from_pkg_info():
-    metadata = distutils.dist.DistributionMetadata("PKG-INFO")
-    return metadata.version
-
-
-def get_version_from_pyver():
-    try:
-        import pyver
-    except ImportError:
-        if 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
-            raise ImportError('You must install pyver to create a package')
-        else:
-            return 'noversion'
-    version, version_info = pyver.get_version(pkg="datalake", public=True)
-    return version
+import versioneer
 
 
 def get_version():
     if os.path.exists("PKG-INFO"):
-        return get_version_from_pkg_info()
+        metadata = distutils.dist.DistributionMetadata("PKG-INFO")
+        return metadata.version
     else:
-        return get_version_from_pyver()
+        return versioneer.get_version()
 
 
 setup(name='datalake',
       url='https://github.com/planetlabs/datalake',
       version=get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='datalake: a metadata-aware archive',
       author='Brian Cavagnolo',
       author_email='brian@planet.com',
       packages=find_packages(exclude=['test']),
       install_requires=[
-          'pyver>=1.0.18',
           'boto>=2.38.0',
           'memoized_property>=1.0.1',
           'simplejson>=3.3.1',
