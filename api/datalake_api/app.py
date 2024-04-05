63a10caab8922d01c967dd87cfdeb7488ed711ed
Alex Bednarek <alex.bednarek@planet.com>
2024-04-05 14:24:40 -0400

    Sentry sampling rate addition.

--- api/datalake_api/app.py
+++ api/datalake_api/app.py
@@ -40,7 +40,8 @@ if level is not None and not app.debug:
 logging.getLogger('boto3.resources.action').setLevel(logging.WARN)
 
 dsn = os.environ.get("SENTRY_DSN")
-traces_sample_rate = float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE"))
+traces_sample_rate = \
+    float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE"), 0) or None
 sentry_sdk.init(dsn=dsn,
                 integrations=[FlaskIntegration()],
                 traces_sample_rate=traces_sample_rate)
