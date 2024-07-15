981af8abb15560f9116b6d6239711be9c40a7c0c
Brian Cavagnolo <brian@planet.com>
2024-07-15 09:32:56 -0700

    fix infinity quotes

--- client/datalake/queue.py
+++ client/datalake/queue.py
@@ -222,7 +222,7 @@ class Uploader(DatalakeQueueBase):
 
     def _update_time_remaining(self):
         if self._run_time_remaining is self.INFINITY:
-            return float(inf)
+            return float('inf')
         now = time.time()
         duration = now - self._run_start
         self._run_time_remaining -= duration
